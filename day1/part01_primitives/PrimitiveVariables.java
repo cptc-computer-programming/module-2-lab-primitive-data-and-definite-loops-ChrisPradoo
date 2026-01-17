407d64bf824f83e0ab11d1979755abe216235754
Christian Castañeda Prado <ChristianPrado5@outlook.com>
2026-01-16 21:49:25 -0800

    Added code to day 1 primitives. Extra practice/ re did since we had problems on day 1.

--- day1/part01_primitives/PrimitiveVariables.java
+++ day1/part01_primitives/PrimitiveVariables.java
@@ -3,36 +3,54 @@ package day1.part01_primitives;
 public class PrimitiveVariables {
 
     public static void main(String[] args) {
-
-        // TODO 1a: declare an int value and do not initialize it yet:
+        // declare an int value and do not initialize it yet:
         //   int a;
+        int value1;
+        
+        // declare and inititalize another int. Give your first int a value
+        // a = 7
+        // int b = 2;
+        value1 = 10;
+        int value2 = 5;
 
         
-        // TODO 1b: declare and inititalize another int. Give your first int a value
-        //   a = 7
-        //   int b = 2;
+
+        // print integer division result (a / b) with a label
+        int intResults = value1 / value2;
+        System.out.println("Results from integer division value 1 and value 2: " + intResults);
         
 
-        // TODO 2: print integer division result (a / b) with a label
+        // print double division result ((double) a / b) with a label
+        double doubleResults = value1 / value2;
+        System.out.println("Results now from double divison value 1 and 2: " + doubleResults);
+        
         
 
-        // TODO 3: print double division result ((double) a / b) with a label
-        
-
-        // TODO 4: declare a double named price and an int named quantity
+        // declare a double named price and an int named quantity
         //         initialize them to any values you choose
+        double price = 10;
+        int quantity = 10;
         
         
-        // TODO 5: compute total cost (double) and print it with a label
+        // compute total cost (double) and print it with a label
+        
+        double totalCost = price * quantity;
+        System.out.println("Total cost: " + totalCost);
+
+        // declare a char variable and print the char and its int code
+
+        char letter = 'A';
+        System.out.println(letter);
+
         
 
-        // TODO 6: declare a char variable and print the char and its int code
-        
-
-        // TODO 7:
         // Modify an existing int variable once using a unary operator (++ or --).
         // Modify it once using an augmented assignment operator (+=, -=, *=, or /=).
         // Print the value after each modification.
+
+        System.out.println("Existing variable i chose to use is quantity, value before: " + quantity);
+        System.out.println("Value after modifying with ++ unary operator: " + ++quantity);
+        System.out.println("Modifying it now with an augmented operator: " + (quantity += 10));
         
     }
 }
