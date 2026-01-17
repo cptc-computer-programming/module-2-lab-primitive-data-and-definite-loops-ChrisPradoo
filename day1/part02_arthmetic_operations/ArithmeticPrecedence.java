18cdb26e99cac6d607dd3c8ea473bc5e68ef2c7f
Christian Castañeda Prado <ChristianPrado5@outlook.com>
2026-01-16 22:18:34 -0800

    Updated day 1 arthmetic operations.

--- day1/part02_arthmetic_operations/ArithmeticPrecedence.java
+++ day1/part02_arthmetic_operations/ArithmeticPrecedence.java
@@ -4,34 +4,59 @@ public class ArithmeticPrecedence {
 
     public static void main(String[] args) {
 
-        // TODO 1:
+        
         // Declare and initialize two int variables.
         // Choose values that will NOT divide evenly.
+        int unEven1 = 5;
+        int unEven2 = 3;
 
-        // TODO 2:
+       
         // Compute and print each result with a label:
         // a + b
         // a - b
         // a * b
         // a / b
+        int adding = unEven1 + unEven2;
+        System.out.println("Adding results: " + adding);
 
-        // TODO 3:
+        int subract = unEven1 - unEven2;
+        System.out.println("Subracting results: " + subract);
+
+        int multiply = unEven1 * unEven2;
+        System.out.println("Multiplying results: " + multiply);
+
+        int divide = unEven1 / unEven2;
+        System.out.println("Dividing results: " + divide);
+
+
+
+       
         // Repeat the division, but force a double result.
         // Do NOT change the variable types.
 
-        // TODO 4:
+        System.out.println("forced double results: " + (double) divide);
+
         // Declare a double variable.
         // Combine it in an arithmetic expression with an int.
         // Print the result and observe the type of the expression.
 
-        // TODO 5:
+        double doubleExperiment = 5;
+        int intExperiment = 4;
+        System.out.println("Combing? not sure what its asking exactly, added both values and it prints as a double: " + doubleExperiment + intExperiment);
+
+
         // Write ONE expression that uses:
         // +, -, *, /, and parentheses.
         // Print the result.
-
+        double resultParanthesis = ((10 + 5) * 2 - (20/5));
+        System.out.println("Result with paraenthesis: " + resultParanthesis);
+        
         // TODO 6:
         // Rewrite the expression above WITHOUT parentheses.
         // Print the result.
+
+        double resultNoParanthesis = (10 + 5 * - 20/5);
+        System.out.println("Result without paranthesis: " + resultNoParanthesis);
 
     }
 }
