0d2e3642aaf12e8b02d3e195f181dddd8bb23613
Christian Castañeda Prado <ChristianPrado5@outlook.com>
2026-01-16 22:46:12 -0800

    Updated day 1, for loops drills.

--- day1/part03_for_loop_drills/ForLoopDrills.java
+++ day1/part03_for_loop_drills/ForLoopDrills.java
@@ -4,17 +4,48 @@ public class ForLoopDrills {
     
     public static void main(String[] args) {
 
-        // TODO 1: print numbers 1..10 on one line
+        // print numbers 1..10 on one line
+        for (int i = 1; i <= 10; i++) {
+            System.out.print(i + " ");
+        }
 
-        // TODO 2: print numbers 10..1 on one line
+        System.out.println();
 
-        // TODO 3: print even numbers 2..20 on one line
+        // print numbers 10..1 on one line
+        for (int i = 10; i >= 1; i--) {
+            System.out.print(i + " ");
+        }
 
-        // TODO 4: print multiples of 5 from 5..50 on one line
+        System.out.println();
 
-        // TODO 5: print powers of 2 (1, 2, 4, 8, 16, 32) on one line
+        // print even numbers 2..20 on one line
+        for (int i = 2; i <= 20; i += 2) {
+            System.out.print(i + " ");
+        }
 
-        // TODO 6: print 20 '*' characters on one line (use a loop)
+        System.out.println();
+
+        // print multiples of 5 from 5..50 on one line
+
+        for (int i = 5; i <= 50; i += 5) {
+            System.out.print(i + " ");
+        }
+
+        System.out.println();
+
+        // print powers of 2 (1, 2, 4, 8, 16, 32) on one line
+
+        for (int i = 1; i <= 32; i *= 2) {
+            System.out.print(i + " ");
+        }
+
+        System.out.println();
+
+        // print 20 '*' characters on one line (use a loop)
+
+        for (int i = 0; i <= 20; i++) {
+            System.out.print("*");
+        }
 
     }
 
